df6b4aa4ed443a1e977108e12ecd35d198c1aeb2
Umidjon <umidjon.abduxamidov@fido.uz>
2023-08-14 11:11:00 +0500

    Interface 3(Dependency inversion)

---
+++ src/oop/polymorphism/interfaces/dependency_inversion/DogZoo.java
@@ -0,0 +1,23 @@
+package oop.polymorphism.interfaces.dependency_inversion;
+
+import oop.inheritance.lesson9.Shepherd;
+
+public class DogZoo {
+    private Dog dog1;
+    private Dog dog2;
+
+
+    public DogZoo(Dog dog1, Dog dog2) {
+        System.out.println("Constructor for any dog");
+        this.dog1 = dog1;
+        this.dog2 = dog2;
+    }
+
+    public DogZoo(Poodle poodle, Shepherd shepherd){
+        System.out.println("Coonstructor for poodle and shephard");
+        this.dog1 = poodle;
+        this.dog2 = shepherd;
+    }
+
+
+}
